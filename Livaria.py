57c068115f22e4fe701f37a5d1407c71afaa2fe3
unknown <2324290116@ACADEMICO.IESB.BR>
2025-06-02 09:44:24 -0300

    first commit

---
+++ Livaria.py
@@ -0,0 +1,12 @@
+Livros = {'O Vento Levou': {'id': 1, 'autor': 'Margaret Mitchell', 'Disponovel': True},
+          'O Senhor dos Anéis': {'id': 2, 'autor': 'J.R.R. Tolkien','Disponovel': True},
+          '1984': {'id': 3, 'autor': 'George Orwell', 'Disponivel': True}
+          },
+
+Usuarios = {2546:{'id': 1,'nome': 'Jorge Matos', 'livros': []},
+            2547:{'id': 2,'nome': 'Ana Silva', 'livros': []},
+           2548:{'id': 3,'nome': 'Carlos Pereira', 'livros': []}
+          }
+
+print(Livros)
+print(Usuarios)
